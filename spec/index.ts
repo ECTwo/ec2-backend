6e5d153f4d3e68b90ae51314ab257cd2f1405869
key kim <keyhyuk.kim@gmail.com>
2020-09-23 00:53:29 +0900

    Fix CI

--- spec/index.ts
+++ spec/index.ts
@@ -3,6 +3,7 @@ import Jasmine from 'jasmine';
 import dotenv from 'dotenv';
 import commandLineArgs from 'command-line-args';
 import logger from '@shared/Logger';
+import { exit } from 'process';
 
 // Setup command line options
 const options = commandLineArgs([
---
+++ spec/tests/default.spec.ts
@@ -0,0 +1,7 @@
+describe('EC2 Test', () => {
+    describe("test example", () => {
+        it('please write down a test! :)', async () => {
+            expect('1').toEqual('1');
+        });
+    });
+});
