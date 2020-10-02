78ab9cf6d52933ca3b7843622ad5000432de36a1
key kim <keyhyuk.kim@gmail.com>
2020-10-03 00:06:26 +0900

    Add get all security group api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UserRouter from './Users';
-
+import AwsRouterV1 from './v1/Aws';
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use('/users', UserRouter);
+router.use('/v1/aws', AwsRouterV1)
 
 // Export the base-router
 export default router;
---
+++ src/routes/v1/Aws.ts
@@ -0,0 +1,27 @@
+import PromiseType from '@entities/PromiseStatus';
+import { Request, Response, Router } from 'express';
+import { execShellCommand } from "../../helper/shell";
+
+// Init shared
+const router = Router();
+
+
+router.get('/securitygroups', async (req: Request, res: Response) => {
+    const { region, access_key, secret_key } = req.body;
+   
+    let ret:PromiseType = await execShellCommand(`docker-compose -f helper/docker-compose.yml run \
+    -e AWS_DEFAULT_REGION=${region} \
+    -e AWS_ACCESS_KEY_ID=${access_key} \
+    -e AWS_SECRET_ACCESS_KEY=${secret_key} \
+    --rm aws ec2 describe-security-groups \
+    `);
+    if (ret.result == 1 || ret.result == 2) {
+        return res.status(400).end(ret.message);
+    }
+
+    return res.status(200).send(JSON.parse(ret.message));
+});
+
+
+export default router;
+
