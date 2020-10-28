f9d5561e4b675f21d148794562523ab226efd007
key kim <keyhyuk.kim@gmail.com>
2020-10-29 01:02:50 +0900

    scaffold

--- src/routes/v1/terraform/Aws.ts
+++ src/routes/v1/terraform/Aws.ts
@@ -1,8 +1,17 @@
 import { Request, Response, Router } from 'express';
 import PromiseType from '@entities/PromiseStatus';
 import { execShellCommand } from "../../../helper/shell";
-
+import logger from '@shared/Logger';
 const router = Router();
+
+router.get('/:user', async (req: Request, res: Response) => {
+    const user = req.params.user;
+    const workspace = req.query.workspace;
+
+    logger.info(`input value = ${user}, ${workspace}`);
+
+    return res.status(200).end();
+});
 
 router.post('/arch/crud', async (req: Request, res: Response) => {
     const { region, subnet, cidr_block, access_key, secret_key } = req.body;
