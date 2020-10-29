6070798cd77d82eaaca3393cbfe583467d54ee51
NLOG <sorisem4106@naver.com>
2020-10-29 22:34:40 +0900

    Fix #5, 커맨드 인젝션을 막기 위한 access_key, secret_key 검증 과정 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,19 @@ import AwsTerraformRouterV1 from './v1/terraform/Aws';
 // Init router and path
 const router = Router();
 
+router.use((req, res, next) => {
+    const { access_key, secret_key } = req.headers;
+    if (typeof access_key === 'string' && !access_key.match(/^[A-Za-z0-9]+$/)) {
+        res.status(400).end(JSON.stringify({}));
+        return;
+    }
+    if (typeof secret_key === 'string' && !secret_key.match(/^[A-Za-z0-9]+$/)) {
+        res.status(400).end(JSON.stringify({}));
+        return;
+    }
+    next();
+});
+
 // Add sub-routes
 router.use('/users', UserRouter);
 router.use('/v1/aws', AwsRouterV1)
