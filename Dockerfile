0a8f767dfc4a0bf5016fbf27b19b08932513f6fb
key kim <keyhyuk.kim@gmail.com>
2020-11-05 23:06:41 +0900

    Update dockerfile && terraform API

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,39 @@
-FROM alpine
+FROM alpine:3.10
+
+ENV TERRAFORM_VERSION=0.13.4
+
 LABEL maintainer "keyhyuk.kim@gmail.com"
 
 SHELL ["/bin/sh", "-c"]
 USER root
 WORKDIR /ec2_backend
+
 COPY . .
+
 
 RUN apk update && \
     apk add git && \
     apk add npm && \
-    apk add terraform
-RUN npm install
+    apk add curl jq python bash ca-certificates git openssl unzip wget && \
+    cd /tmp && \
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
+    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/bin && \
+    wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip -O /tmp/google-cloud-sdk.zip && \
+    cd /usr/local && unzip /tmp/google-cloud-sdk.zip && \
+    google-cloud-sdk/install.sh --usage-reporting=false --path-update=true --bash-completion=true && \
+    google-cloud-sdk/bin/gcloud config set --installation component_manager/disable_update_check true && \
+    rm -rf /tmp/* && \
+    rm -rf /var/cache/apk/* && \
+    rm -rf /var/tmp/*
+
+ENV PATH = $PATH:/usr/local/google-cloud-sdk/bin/
+
+RUN npm install && \
+    git submodule init && \
+    git submodule update --remote
+
+ENV SRC /ec2_backend/terraform-boilerplate
+
 CMD ["npm", "run", "start:dev", "2>/dev/null"]
 
 
--- src/routes/v1/terraform/Aws.ts
+++ src/routes/v1/terraform/Aws.ts
@@ -2,7 +2,82 @@ import { Request, Response, Router } from 'express';
 import PromiseType from '@entities/PromiseStatus';
 import { execShellCommand } from "../../../helper/shell";
 import logger from '@shared/Logger';
+const fs = require('fs');
+
 const router = Router();
+
+router.post('/:user', async (req: Request, res: Response) => {
+    // get data
+    const publicData = {
+        server: [{
+          name: "ec2_public_1",
+          cpu: 0,
+          ram: 0,
+          os: "t3.micro"
+        },
+        {
+          name: "ec2_public_2",
+          cpu: 0,
+          ram: 0,
+          os: "t3.micro"
+        } 
+    ]}
+    const privateData = {
+        server: [{
+          name: "ec2_private_1",
+          cpu: 0,
+          ram: 0,
+          os: "t3.micro"
+        },
+        {
+          name: "ec2_private_2",
+          cpu: 0,
+          ram: 0,
+          os: "t3.micro"
+        },
+    ]}
+    const dbData = {
+        server: [{
+          name: "bobdb",
+          lgType: "mysql",
+          masterID: "bobmasterid",
+          masterPW: "%bobmasterpassword1234%"
+        },
+    ]}
+
+    // dataset
+
+    const user = req.params.user;
+    const { access_key, secret_key } = req.headers;
+    req.setTimeout(1000 * 1 * 60 * 5)
+    
+    // ws의 결과, 이미 존재하는 workspace라면 workspace를 수행하지 않습니다
+    // issue #
+    try {
+      await execShellCommand(`terraform workspace new ${user}_arch`);
+    }
+    catch(error) {
+      logger.info(error)
+    }
+    
+    logger.info(`${process.env.SRC}`)
+    
+    await execShellCommand(`terraform init ${process.env.SRC}/terraform/arch/bob/only_ec2`);
+
+    await execShellCommand(`terraform apply -auto-approve \
+                            -var "region=us-east-2" \
+                            -var "access_key=${access_key}" \
+                            -var "secret_key=${secret_key}" \
+                            -var "public_cnt=${publicData.server.length}" \
+                            -var "private_cnt=${privateData.server.length}" \
+                            ${process.env.SRC}/terraform/arch/bob/only_ec2`)
+
+    fs.readFile(`${process.env.SRC}/../terraform.tfstate.d/${user}_arch/terraform.tfstate`, (err, data) => {
+      if (err) return res.status(200).end();
+  
+      return res.status(200).send(JSON.parse(data));
+    });
+})
 
 router.get('/:user', async (req: Request, res: Response) => {
     const user = req.params.user;
@@ -10,35 +85,24 @@ router.get('/:user', async (req: Request, res: Response) => {
 
     logger.info(`input value = ${user}, ${workspace}`);
 
+
+    // run 
     return res.status(200).end();
 });
 
-router.post('/arch/crud', async (req: Request, res: Response) => {
-    const { region, subnet, cidr_block, access_key, secret_key } = req.body;
-    let ret:PromiseType = await execShellCommand(`docker-compose -f helper/docker-compose.yml run \
-    --rm tf apply -auto-approve \
-    -var "region=${region}" \
-    -var "cidr_block=${cidr_block}" \
-    -var "subnet=${subnet}" \
-    -var "access_key=${access_key}" \
-    -var "secret_key=${secret_key}"
-    `);
 
-    if (ret.result == 1 || ret.result == 2) {
-        return res.status(400).end(ret.message);
-    }
+router.delete('/:user', async (req: Request, res: Response) => {
+    const { access_key, secret_key } = req.headers;
+    const user = req.params.user;
+    req.setTimeout(1000 * 1 * 60 * 5)
 
-    return res.status(201).send(JSON.parse(ret.message));
-})
-
-router.delete('/arch/crud', async (req: Request, res: Response) => {
-    const { access_key, secret_key } = req.body;
-    
-    let ret:PromiseType = await execShellCommand(`docker-compose -f helper/docker-compose.yml run \
-            --rm tf destroy -auto-approve \
+    logger.info('start to delete ' + user)
+    await execShellCommand(`terraform workspace select ${user}_arch`);
+    let ret:PromiseType = await execShellCommand(`terraform destroy -auto-approve \
             -var "access_key=${access_key}" \
-            -var "secret_key=${secret_key}"
-    `);
+            -var "secret_key=${secret_key}" \
+            -var "region=us-east-2" \
+            ${process.env.SRC}/terraform/arch/bob/only_ec2`);
 
     if (ret.result == 1 || ret.result == 2) {
         return res.status(400).end(ret.message);
